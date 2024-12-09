5af0ba4bd34cec023817e16f2d6a9c540f1f3a38
Jean Marcele <jeanmarcelamoutche774@gmail.com>
2024-12-09 02:55:12 -0500

    Initial commit

---
+++ routes/xxxx.php
@@ -0,0 +1,11 @@
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
+
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
+Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+Route::get('payment/show', [PaymentController::class, 'show'])->name('payment.show');
